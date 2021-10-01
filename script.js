9b42f77a546b9858f540c84a5e562bda0b03a986
Brady-Palmer <bradypalmer28@gmail.com>
2021-10-01 15:41:40 +0000

    i was wrong shalom was right

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ function setup() {
     }
 
 function changeColor() {
-    var list_entries = ["portfolio-1", "portfolio-2", "portfolio-3"]
-    var list = document.getElementById("portfolio li");
-    list.style.background = colors[counter];
+    //var list_entries = ["portfolio-1", "portfolio-2", "portfolio-3"]
+    var list = document.getElementsByClassName("portfolio-li");
+    for(var a=[], i=list.length; i;) {
+        a[--i]=list[i];
+    }
+    a.forEach(element => element.style.background=colors[counter]);
+
+    var link = document.getElementsByClassName("portfolio-a");
+    a.forEach(element => element.style.background=colors[counter]);
     
     if (counter === colors.length - 1) {
         counter = 0;
