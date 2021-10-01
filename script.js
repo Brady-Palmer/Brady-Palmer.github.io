e5086eda11aff10f780480076a8db94c354c2d5c
Brady-Palmer <bradypalmer28@gmail.com>
2021-10-01 14:32:30 +0000

    working button shalom was wrong

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+
 var counter = 0;
-var colors = ["red", "purple", "blue", "green", "yellow", "orange"];
+var colors = ["purple", "blue", "green", "yellow", "orange", "red"];
 
 function setup() {
     setInterval(changeColor, 1500);
     }
 
 function changeColor() {
+    var list_entries = ["portfolio-1", "portfolio-2", "portfolio-3"]
     var list = document.getElementById("portfolio li");
     list.style.background = colors[counter];
     
-    if (counter === 5) {
+    if (counter === colors.length - 1) {
         counter = 0;
     } else {
         counter++;
